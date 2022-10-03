3466748fc2243250f412693141e2e8590d00ae12
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-10-03 09:46:16 -0500

    envio con pruebas

--- EjercicioPolinomio.js
+++ EjercicioPolinomio.js
@@ -96,4 +96,23 @@ funcionPolinomio=(a,b,c)=>{
    return false;
  } 
 }
-funcionPolinomio(1,2,-8);
+
+// 1.el valor de A debe ser distinto de cero 
+funcionPolinomio(0,2,3);
+
+// 2.el valor ingresado no es un numero 
+funcionPolinomio("a",2,3);
+
+// 3. el discriminante negativo 
+funcionPolinomio(1,1,1);
+// delta negativo 
+
+//4. a=1 b=3 c=2 
+funcionPolinomio(1,3,2);
+
+//4. a=5 b=-20 c=15 
+funcionPolinomio(5,-20,15);
+
+// Ingreso de valores  
+// funcionPolinomio(1,2,-8);
+
