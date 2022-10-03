7b26f7f7b94aaaee0435d0c935dc165fade06587
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-10-03 09:17:44 -0500

    envio ejercicio

---
+++ EjercicioPolinomio.js
@@ -0,0 +1,99 @@
+// declaracion de variables
+var a=0 ;
+var b =0;
+var c =0;
+// 
+var aCorrect=0 ;
+var aCorrect =0;
+var cCorrect =0;
+var delta;
+var res ;
+
+
+// Funciones 
+
+
+// funion de validacion de tipo de numero
+funcionVal=(dato)=>{
+ try{
+    if (typeof(dato) =='number' ){
+		  console.log('dato numerico')
+      return true;
+    	
+  }
+   // return true ;
+  
+ }catch(error){
+  	console.log('error de dato no valido ');
+   return false;
+ }
+}
+// caculo de las raices
+calcularRaices=(a,b,c)=>{
+  delta = (parseInt(b) * parseInt(b)) - (((4 * parseInt(a) )* parseInt(c))); 
+  // console.log("valor de dato");
+  console.log(delta);
+  if (delta < 0)  
+  {  
+   console.log("Sin soluciones"); 
+  }  
+  else  
+  {  
+     if (delta == 0) 
+     {  
+           console.log("Solución única: " + (-b / (2 * a))); 
+     } 
+     else 
+     { 
+            console.log("Solución n.°1: " + (-b +  Math.sqrt(delta))/(2*a)) ;
+       			console.log("Solución n.°2: " + (b +  Math.sqrt(delta))/(2*a)) ;
+       // 
+         		// console.log("Solución n.°2: "  (b*b +  Math.sqrt(delta))/2*(a)) ;
+     }
+  }
+}
+
+
+
+
+
+
+
+
+funcionPolinomio=(a,b,c)=>{
+
+ try{
+    (funcionVal(a)== true)?
+      aCorrect=a
+   	:console.log('dato invalido,revise los datos ingresados');
+   (funcionVal(b)== true)?
+      bCorrect=b
+   	:console.log('dato invalido,revise los datos ingresado');
+   (funcionVal(c)== true)?
+      cCorrect=c
+   	:console.log('dato invalido,revise los datos ingresado');
+   // ,cCorrect
+   if ( aCorrect !=0 ){
+     				// proceso de cacular 
+     				calcularRaices(aCorrect,bCorrect,cCorrect);
+     
+     
+     
+    }else{
+      console.log('ingrese un valor diferente de 0')
+    }
+      // return false
+   console.log(aCorrect)
+  	// Proceso para calcular
+   
+
+
+   
+     // return true ;
+  
+ }catch(error){
+  	console.log('error del proceso');
+   return false;
+ } 
+}
+funcionPolinomio(1,1,1);
